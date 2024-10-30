29acd90991053f0f9a942a8c38d827d106b135b5
samuel s <samuelshimels3@gmail.com>
2024-10-30 11:11:39 +0300

    updating total ratings counter

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -8,15 +8,20 @@ class ContentRating extends Component {
     this.state = {
       likes: 0,
       dislikes: 0,
+      totalRatings: 0,
       handleLike: () => {
         this.setState((prevState) => ({
-          likes: prevState.likes + 1
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
         }));
 
       },
       handleDislike: () => {
         this.setState((prevState) => ({
-          dislikes: prevState.dislikes + 1
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+
+
         }));
       }
     }
@@ -35,6 +40,9 @@ class ContentRating extends Component {
         <button className="dislike-button" onClick={this.state.handleDislike}>
           Dislike ({this.state.dislikes})
         </button>
+        <div className='totalRatings'>
+          <p>Total Ratings is : {this.state.totalRatings}</p>
+        </div>
       </div>
      </div>
      </>
